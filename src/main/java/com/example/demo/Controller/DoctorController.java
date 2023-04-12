40981658f3cd8871b7b340725a7b5c1dc6d5c464
hoaian1232812 <124787635+hoaian1232812@users.noreply.github.com>
2023-04-12 10:58:29 +0700

    Add files via upload

---
+++ src/main/java/com/example/demo/Controller/DoctorController.java
@@ -0,0 +1,36 @@
+package com.example.demo.Controller;
+
+import com.example.demo.model.Doctor;
+import com.example.demo.model.ResponseObject;
+import com.example.demo.repositories.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "api/v1")
+public class DoctorController {
+    @Autowired
+    private DoctorRepository repository;
+    @GetMapping("Doctors/{departmentId}")
+    public List<Doctor> getAllDoctorByDepartmentId(@PathVariable String departmentId){
+        return repository.findDoctorsByDepartmentId(departmentId);
+    }
+    @GetMapping("/Doctor/{id}")
+    public ResponseEntity<ResponseObject> getDoctorById(@PathVariable String id){
+        Optional<Doctor> d = repository.findById(id);
+        return d.isPresent()? ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Query product successfully", d)
+        ) : ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new ResponseObject("false", "Can't not find doctor with id = " + id, "")
+        );
+    }
+}
