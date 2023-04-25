eadf8df41f47e18eb989a5336ecb7f21b341f9d9
HoThanhHoaiAn <20130193@st.hcmuaf.edu.vn>
2023-04-25 22:07:43 +0700

    update

--- src/main/java/com/example/demo/repositories/DoctorRepository.java
+++ src/main/java/com/example/demo/repositories/DoctorRepository.java
@@ -2,13 +2,11 @@ package com.example.demo.repositories;
 
 import com.example.demo.model.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, String> {
-    @Query("select d from Doctor d where d.department.id=:department_id")
-    List<Doctor> findByDepartmentId(@Param("department_id") String department_id);
+    public List<Doctor> findByDepartmentId(@Param("department_id") String department_id);
 }
