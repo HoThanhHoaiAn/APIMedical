98c612b21052262ad74675cf3e76eaee3c69e2da
HoThanhHoaiAn <20130193@st.hcmuaf.edu.vn>
2023-04-25 16:24:13 +0700

    update

--- src/main/java/com/example/demo/repositories/DoctorRepository.java
+++ src/main/java/com/example/demo/repositories/DoctorRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, String> {
-    List<Doctor> findDoctorsByDepartmentId(String department_id);
+    @Query
+    List<Doctor> findByDepartmentId(String department_id);
 }
