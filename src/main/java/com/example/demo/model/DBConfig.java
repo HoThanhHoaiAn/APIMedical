b54fb825ca72aac1d6e53d54d2a95625b0ab49db
HoThanhHoaiAn <20130193@st.hcmuaf.edu.vn>
2023-04-25 17:08:43 +0700

    update

--- src/main/java/com/example/demo/model/DBConfig.java
+++ src/main/java/com/example/demo/model/DBConfig.java
@@ -13,9 +13,9 @@ public class DBConfig{
        public DataSource dataSource(){
           DriverManagerDataSource dataSource = new DriverManagerDataSource();
           dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-          dataSource.setUrl("your url");
-          dataSource.setUsername( "username" );
-          dataSource.setPassword( "password" );
+          dataSource.setUrl("jdbc:mysql://containers-us-west-3.railway.app:6196/railway");
+          dataSource.setUsername( "root" );
+          dataSource.setPassword( "Z9kl1YxIk4DdQkIHOIuw" );
           return dataSource;
        }
 
